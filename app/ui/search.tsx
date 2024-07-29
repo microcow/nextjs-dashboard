b233372676e525dc84ecb726c26775769add8ea8
microcow <wooo2385@gmail.com>
2024-07-29 15:13:17 +0900

    072992

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams(); // useSearchParams() 메서드의 return값을 searchParams에 저장
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams); // params에 새로운 URLSearchParams 객체를 저장
@@ -13,6 +15,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     } else {
       params.delete('query');
     }
+    replace(`${pathname}?${params.toString()}`);
   }
 
   return (
